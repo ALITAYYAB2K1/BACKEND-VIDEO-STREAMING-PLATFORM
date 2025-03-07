2faa68f7d906f477f5ab206e6258f39d2c2c51bd
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-07 07:08:35 +0500

    more changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "./env",
 });
-
 import connectDB from "./db/index.js";
-connectDB();
+import app from "./app.js";
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to database- connection failed: ", error);
+    process.exit(1);
+  });
