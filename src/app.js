3b47b57952878d0c3f41298edbb548b7d92d0de2
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-07 07:43:00 +0500

    readme

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(cookieParser());
+app.use(
+  express.json({
+    limit: "20kb",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
