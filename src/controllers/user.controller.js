36440e3d15fc05d140775bdb38bdbbf1a3697692
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-10 18:05:02 +0500

    add getUserChannelProfile function to retrieve channel details and subscriber counts

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -324,6 +324,81 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse(200, user, "User cover image updated"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  //steps - algorithm
+  // get user from username
+  // check if user exists
+  // get user details
+  // get user videos
+  // get user subscribers
+  // get user subscriptions
+  // get user subscribers count
+  // get user subscriptions count
+  // return response if not send error
+  //--------------------------------------------------------------------------------
+  const { username } = req.params;
+  if (!username) {
+    throw new ApiError(400, "Username is mandatory");
+  }
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: { $size: "$subscribers" },
+        subscribedToCount: { $size: "$subscribedto" },
+      },
+      isSubscribed: {
+        $cond: {
+          if: {
+            $in: [req.user?._id, "$subscribers.subscriber"],
+          },
+          then: true,
+          else: false,
+        },
+      },
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        subscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel not found");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "Channel found"));
+});
+
 export {
   registerUser,
   loginUser,
@@ -334,4 +409,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
